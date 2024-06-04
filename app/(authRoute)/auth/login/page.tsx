e4d1e3eef9be31bba167d9206f3a16403c7e09e3
mihirgala <mihirgala33@gmail.com>
2024-06-04 11:20:15 +0530

    Added db and some routing layouts

---
+++ app/(authRoute)/auth/login/page.tsx
@@ -0,0 +1,6 @@
+const LoginPage = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">LoginPage</div>
+  )
+}
+export default LoginPage
---
+++ app/(normalRoute)/page.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <section>
+        <h1>Home Page</h1>
+      </section>
+    </main>
+  )
+}
+export default HomePage
