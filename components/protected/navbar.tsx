ed39f2fa8517fc34a97028ec1835681354f8e096
mihirgala <mihirgala33@gmail.com>
2024-06-06 17:34:31 +0530

    minor layout changes to make mobile ui better

--- components/protected/navbar.tsx
+++ components/protected/navbar.tsx
@@ -8,7 +8,7 @@ interface ProtectedNavbarProps {
 }
 export const ProtectedNavbar = ({user}:ProtectedNavbarProps) => {
   return (
-    <header className="sm:px-5 sm:space-between flex items-center select-none">
+    <header className="px-5 justify-between md:justify-normal flex items-center select-none">
         <span className="md:mx-auto"><Link href={"/"}><Logo/></Link></span>
         <UserButton user={user}/>
     </header>
