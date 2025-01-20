12cde23922df1df0a29fd0be9c344f00d8b231b8
Jay P <jay@sorbet.studio>
2025-01-20 22:30:19 +0530

    attempt at fixing a type error

--- app/(protectedRoute)/chat/[chatId]/page.tsx
+++ app/(protectedRoute)/chat/[chatId]/page.tsx
@@ -1,30 +1,38 @@
-import { ChatBotComponent } from "@/components/chat/chat-bot"
-import { siteConfig } from "@/config"
-import { getChatById, getMessagesByChatId } from "@/data"
-import { currentUser } from "@/lib/auth"
-import { Metadata } from "next"
-import { redirect } from "next/navigation"
+import { ChatBotComponent } from "@/components/chat/chat-bot";
+import { siteConfig } from "@/config";
+import { getChatById, getMessagesByChatId } from "@/data";
+import { currentUser } from "@/lib/auth";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+
+export const runtime = "edge";
 
 export const metadata: Metadata = {
-    title: `Chat | ${siteConfig.name}`,
-    description: "Chat Page",
-}
+  title: `Chat | ${siteConfig.name}`,
+  description: "Chat Page",
+};
 
-// export const runtime = 'edge'
+type ChatPageProps = {
+  params: {
+    chatId: string;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-interface ChatPageProps {
-    params: { chatId: string }
-}
 const ChatPage = async ({ params }: ChatPageProps) => {
-    const user = await currentUser()
-    const { chatId } = params
-    const chat = await getChatById(chatId)
-    const dbMessages = await getMessagesByChatId(chatId)
-    if (!chat || user?.id !== chat.userId) return redirect("/")
-    return (
-        <div className="md:w-[80%] md:mx-auto">
-            <ChatBotComponent chatId={chatId} isChatName={!!chat?.name} dbMessages={dbMessages!} />
-        </div>
-    )
-}
-export default ChatPage
+  const user = await currentUser();
+  const { chatId } = params;
+  const chat = await getChatById(chatId);
+  const dbMessages = await getMessagesByChatId(chatId);
+  if (!chat || user?.id !== chat.userId) return redirect("/");
+  return (
+    <div className="md:w-[80%] md:mx-auto">
+      <ChatBotComponent
+        chatId={chatId}
+        isChatName={!!chat?.name}
+        dbMessages={dbMessages!}
+      />
+    </div>
+  );
+};
+export default ChatPage;
