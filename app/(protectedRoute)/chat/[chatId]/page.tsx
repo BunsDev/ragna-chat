4e1a9ccd1911e595c3cfe3112a89b4ff55d7d9fb
Jay P <jay@sorbet.studio>
2025-01-20 23:21:29 +0530

    attempted fix #3

--- app/(protectedRoute)/chat/[chatId]/page.tsx
+++ app/(protectedRoute)/chat/[chatId]/page.tsx
@@ -16,7 +16,6 @@ type ChatPageProps = {
   params: {
     chatId: string;
   };
-  searchParams: { [key: string]: string | string[] | undefined };
 };
 
 const ChatPage = async ({ params }: ChatPageProps) => {
