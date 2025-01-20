7f1f9c9e142f7bbec702f40666c05998e2a5d2e2
Jay P <jay@sorbet.studio>
2025-01-20 21:56:39 +0530

    fixed bugs and updated packages

--- app/(normalRoute)/try/chat/[chatId]/page.tsx
+++ app/(normalRoute)/try/chat/[chatId]/page.tsx
@@ -1,28 +1,31 @@
-import { TrialChatBotComponent } from "@/components/try/chat-bot"
-import { siteConfig } from "@/config"
-import { getTrialChatById, getTrialMessagesByChatId } from "@/data"
-import { Metadata } from "next"
-import { redirect } from "next/navigation"
+import { TrialChatBotComponent } from "@/components/try/chat-bot";
+import { siteConfig } from "@/config";
+import { getTrialChatById, getTrialMessagesByChatId } from "@/data";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: `Trial Chat | ${siteConfig.name}`,
   description: "Trial Chat Page",
-}
-
-// export const runtime = 'edge'
+};
 
 interface TrialChatBotPageProps {
-  params: { chatId: string }
+  params: Promise<{ chatId: string }>;
 }
+
 const TrialChatBotPage = async ({ params }: TrialChatBotPageProps) => {
-  const { chatId } = params
-  const chat = await getTrialChatById(chatId)
-  const dbMessages = await getTrialMessagesByChatId(chatId)
-  if (!chat) return redirect('/try')
+  const { chatId } = await params;
+  const chat = await getTrialChatById(chatId);
+  const dbMessages = await getTrialMessagesByChatId(chatId);
+  if (!chat) return redirect("/try");
   return (
     <div className="md:w-[80%] md:mx-auto">
-      <TrialChatBotComponent chatId={chatId} isChatName={!!chat?.name} dbMessages={dbMessages!} />
+      <TrialChatBotComponent
+        chatId={chatId}
+        isChatName={!!chat?.name}
+        dbMessages={dbMessages!}
+      />
     </div>
-  )
-}
-export default TrialChatBotPage
+  );
+};
+export default TrialChatBotPage;
